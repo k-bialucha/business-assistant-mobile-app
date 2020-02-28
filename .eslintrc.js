c560de5be3b33d06f1851badc8946799870ba1c6
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-02-28 18:51:45 +0100

    ESLint, Prettier config added

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'no-use-before-define': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+    'arrow-body-style': [2, 'as-needed'],
+    'comma-dangle': [2, 'only-multiline'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+    'import/imports-first': 2,
+    'import/prefer-default-export': 2,
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-uses-vars': 2,
+    'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
+    'react/prop-types': 0,
+    'react/state-in-constructor': [2, 'never'],
+    'max-len': 0,
+  },
+};
