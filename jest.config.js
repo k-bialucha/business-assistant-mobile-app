e69442285089f3490a310bdbcbb331a1b5277ad3
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-03-04 19:13:41 +0100

    Jest config extracted to a separate component

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)',
+  ],
+};
+
+module.exports = config;
