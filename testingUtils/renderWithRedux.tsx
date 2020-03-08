d66c1a932005d7a070859aca4aee75494d5a520f
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-03-09 00:33:03 +0100

    Add renderWithRedux utility function

---
+++ testingUtils/renderWithRedux.tsx
@@ -0,0 +1,23 @@
+/**
+ * renderWithRedux
+ *
+ * Testing utility function that wraps element
+ * with redux Provider component.
+ * source: https://testing-library.com/docs/example-react-redux
+ */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from 'react-native-testing-library';
+
+import configureStore from '../redux/configureStore';
+
+function renderWithRedux(ui: JSX.Element) {
+  const store = configureStore();
+
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store,
+  };
+}
+
+export default renderWithRedux;
