8cc86e1ee69861c55fbcbf6ccea964386c632d7b
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-03-17 01:01:30 +0100

    Add auth saga tests

---
+++ redux/auth/saga.test.ts
@@ -0,0 +1,40 @@
+import { delay, put } from 'redux-saga/effects';
+
+import { loginSaga } from './saga';
+import { DOMAIN_NAME, LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE } from './types';
+
+describe(`${DOMAIN_NAME}/reducer`, () => {
+  it('handles successful login', () => {
+    const generator = loginSaga({
+      type: LOGIN,
+      payload: { username: 'some-username', password: 'strong-password' },
+    });
+
+    const delayDescriptor = generator.next().value;
+
+    expect(delayDescriptor).toEqual(delay(1000));
+
+    const putDescriptor = generator.next().value;
+
+    expect(putDescriptor).toEqual(
+      put({ type: LOGIN_SUCCESS, payload: expect.any(String) })
+    );
+  });
+
+  it('handles unsuccessful login', () => {
+    const generator = loginSaga({
+      type: LOGIN,
+      payload: { username: 'some-username', password: 'pass' },
+    });
+
+    const delayDescriptor = generator.next().value;
+
+    expect(delayDescriptor).toEqual(delay(1000));
+
+    const putDescriptor = generator.next().value;
+
+    expect(putDescriptor).toEqual(
+      put({ type: LOGIN_FAILURE, payload: expect.any(String) })
+    );
+  });
+});
