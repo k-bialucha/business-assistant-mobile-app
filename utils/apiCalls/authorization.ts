d088a64bcef6818f401801568788139654ec5820
bastej <siatkasebastian@gmail.com>
2020-04-13 12:01:41 +0200

    Adding function which creating user in firebase auth

--- utils/apiCalls/authorization.ts
+++ utils/apiCalls/authorization.ts
@@ -14,10 +14,36 @@ export const loginUser = (email, password) => {
         returnSecureToken: true,
       }),
     }
-  ).then(data =>
-    data.json().catch(error => {
+  )
+    .then(data => data.json())
+    .catch(error => {
       // TODO: method which throw different error based on request status
       throw new Error(error);
+    });
+};
+
+export const signupUser = (email: string, password: string, phone?: string) => {
+  return fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseApiKey}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+    }
+  )
+    .then(data => {
+      console.log(`Save additional user data like phone ${phone} in db`);
+
+      return data.json();
     })
-  );
+    .catch(error => {
+      // TODO: method which throw different error based on request status
+      throw new Error(error);
+    });
 };
