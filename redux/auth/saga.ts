7ccfaf636fce88498a44fdaaf7506175712ccb0b
bastej <siatkasebastian@gmail.com>
2020-04-13 12:00:04 +0200

    Creating signup saga

--- redux/auth/saga.ts
+++ redux/auth/saga.ts
@@ -1,9 +1,14 @@
 import { delay, put, takeLatest } from 'redux-saga/effects';
 
-import { loginUser } from '../../utils/apiCalls/authorization';
+import { loginUser, signupUser } from '../../utils/apiCalls/authorization';
 
-import { loginFailure, loginSuccess } from './actions';
-import { LOGIN, LoginAction } from './types';
+import {
+  loginFailure,
+  loginSuccess,
+  signupFailure,
+  signupSuccess,
+} from './actions';
+import { LOGIN, LoginAction, SIGNUP, SignupAction } from './types';
 
 export function* loginSaga({ payload: { username, password } }: LoginAction) {
   try {
@@ -19,6 +24,25 @@ export function* loginSaga({ payload: { username, password } }: LoginAction) {
   }
 }
 
+export function* signupSaga({
+  payload: { email, password, phone },
+}: SignupAction) {
+  try {
+    yield delay(1000);
+
+    const response = yield signupUser(email, password, phone);
+
+    console.log('response: ', response);
+
+    const { idToken: token, localId: userId } = response;
+
+    yield put(signupSuccess(token, userId));
+  } catch (error) {
+    yield put(signupFailure(error));
+  }
+}
+
 export default function* watchSaga() {
   yield takeLatest(LOGIN, loginSaga);
+  yield takeLatest(SIGNUP, signupSaga);
 }
