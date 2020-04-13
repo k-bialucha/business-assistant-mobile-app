bb056fd631448145bbbd63673ea320a82697a924
bastej <siatkasebastian@gmail.com>
2020-04-13 21:31:30 +0200

    Adding types to custom text field Component

--- components/form/TextField.tsx
+++ components/form/TextField.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { TextInputProps } from 'react-native';
 
 import { StyledContainer, StyledTextField } from './TextField.styled';
 
-const TextField = props => {
+const TextField: React.SFC<TextInputProps> = props => {
   return (
     <StyledContainer>
-      <StyledTextField {...props} />
+      <StyledTextField
+        {...props}
+        placeholderTextColor="rgba(255,255,255, 0.6)"
+      />
     </StyledContainer>
   );
 };
