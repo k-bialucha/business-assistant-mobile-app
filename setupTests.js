9f9d9371d49e0b72653c3b0e0a688a33a28661b8
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-04-19 22:23:21 +0200

    Remove jest-fetch-mock

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { enableFetchMocks } from 'jest-fetch-mock';
 
 Enzyme.configure({ adapter: new Adapter() });
-enableFetchMocks();
