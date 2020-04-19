209f36f094cf1c4fa026a741fe1d4829c4566453
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-04-19 20:25:39 +0200

    Adjust saga tests

--- redux/auth/saga.test.ts
+++ redux/auth/saga.test.ts
@@ -1,30 +1,36 @@
-import { delay, put } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 
 import { loginSaga } from './saga';
 import { DOMAIN_NAME, LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS } from './types';
 
-describe(`${DOMAIN_NAME}/reducer`, () => {
+describe(`${DOMAIN_NAME}/saga`, () => {
   it('handles successful login', () => {
+    const someApiResponse = {
+      idToken: 'token-123',
+      localId: '12345',
+    };
     const generator = loginSaga({
       type: LOGIN,
       payload: { username: 'some-username', password: 'strong-password' },
     });
 
     const delayDescriptor = generator.next().value;
+    const apiCallDescriptor = generator.next().value;
+    const putDescriptor = generator.next(someApiResponse).value;
 
-    expect(delayDescriptor).toEqual(delay(1000));
-
-    const putDescriptor = generator.next().value;
-
+    expect(delayDescriptor).toMatchSnapshot();
+    expect(apiCallDescriptor).toMatchSnapshot();
     expect(putDescriptor).toEqual(
       put({
         type: LOGIN_SUCCESS,
         payload: { token: expect.any(String), userId: expect.any(String) },
       })
     );
+    expect(generator.next().done).toBe(true);
   });
 
   it('handles unsuccessful login', () => {
+    const someError = new Error('Bad Credentials');
     const generator = loginSaga({
       type: LOGIN,
       payload: { username: 'some-username', password: 'pass' },
@@ -32,12 +38,15 @@ describe(`${DOMAIN_NAME}/reducer`, () => {
 
     const delayDescriptor = generator.next().value;
 
-    expect(delayDescriptor).toEqual(delay(1000));
+    const apiCallDescriptor = generator.next().value;
 
-    const putDescriptor = generator.next().value;
+    const putDescriptor = generator.throw(someError).value;
 
+    expect(delayDescriptor).toMatchSnapshot();
+    expect(apiCallDescriptor).toMatchSnapshot();
     expect(putDescriptor).toEqual(
-      put({ type: LOGIN_FAILURE, payload: expect.any(String) })
+      put({ type: LOGIN_FAILURE, payload: someError.message })
     );
+    expect(generator.next().done).toBe(true);
   });
 });
