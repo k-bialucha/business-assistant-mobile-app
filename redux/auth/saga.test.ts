514926162d248de5c3a1d11199a4e9ecccc4d767
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-04-20 23:31:54 +0200

    Add signupSaga tests

--- redux/auth/saga.test.ts
+++ redux/auth/saga.test.ts
@@ -1,52 +1,109 @@
 import { put } from 'redux-saga/effects';
 
-import { loginSaga } from './saga';
-import { DOMAIN_NAME, LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS } from './types';
+import { loginSaga, signupSaga } from './saga';
+import {
+  DOMAIN_NAME,
+  LOGIN,
+  LOGIN_FAILURE,
+  LOGIN_SUCCESS,
+  SIGNUP,
+  SIGNUP_FAILURE,
+  SIGNUP_SUCCESS,
+} from './types';
 
 describe(`${DOMAIN_NAME}/saga`, () => {
-  it('handles successful login', () => {
-    const someApiResponse = {
-      idToken: 'token-123',
-      localId: '12345',
-    };
-    const generator = loginSaga({
-      type: LOGIN,
-      payload: { username: 'some-username', password: 'strong-password' },
+  describe('loginSaga', () => {
+    it('handles successful login', () => {
+      const someApiResponse = {
+        idToken: 'token-123',
+        localId: '12345',
+      };
+      const generator = loginSaga({
+        type: LOGIN,
+        payload: { username: 'some-username', password: 'strong-password' },
+      });
+
+      const delayDescriptor = generator.next().value;
+      const apiCallDescriptor = generator.next().value;
+      const putDescriptor = generator.next(someApiResponse).value;
+
+      expect(delayDescriptor).toMatchSnapshot();
+      expect(apiCallDescriptor).toMatchSnapshot();
+      expect(putDescriptor).toEqual(
+        put({
+          type: LOGIN_SUCCESS,
+          payload: { token: expect.any(String), userId: expect.any(String) },
+        })
+      );
+      expect(generator.next().done).toBe(true);
     });
 
-    const delayDescriptor = generator.next().value;
-    const apiCallDescriptor = generator.next().value;
-    const putDescriptor = generator.next(someApiResponse).value;
+    it('handles unsuccessful login', () => {
+      const someError = new Error('Bad Credentials');
+      const generator = loginSaga({
+        type: LOGIN,
+        payload: { username: 'some-username', password: 'badpass' },
+      });
 
-    expect(delayDescriptor).toMatchSnapshot();
-    expect(apiCallDescriptor).toMatchSnapshot();
-    expect(putDescriptor).toEqual(
-      put({
-        type: LOGIN_SUCCESS,
-        payload: { token: expect.any(String), userId: expect.any(String) },
-      })
-    );
-    expect(generator.next().done).toBe(true);
+      const delayDescriptor = generator.next().value;
+      const apiCallDescriptor = generator.next().value;
+      const putDescriptor = generator.throw(someError).value;
+
+      expect(delayDescriptor).toMatchSnapshot();
+      expect(apiCallDescriptor).toMatchSnapshot();
+      expect(putDescriptor).toEqual(
+        put({ type: LOGIN_FAILURE, payload: someError.message })
+      );
+      expect(generator.next().done).toBe(true);
+    });
   });
 
-  it('handles unsuccessful login', () => {
-    const someError = new Error('Bad Credentials');
-    const generator = loginSaga({
-      type: LOGIN,
-      payload: { username: 'some-username', password: 'pass' },
+  describe('signupSaga', () => {
+    it('handles successful signup', () => {
+      const someApiResponse = {
+        idToken: 'token-123',
+        localId: '12345',
+      };
+      const generator = signupSaga({
+        type: SIGNUP,
+        payload: {
+          email: 'some-email@example.com',
+          password: 'strong-password',
+        },
+      });
+
+      const delayDescriptor = generator.next().value;
+      const apiCallDescriptor = generator.next().value;
+      const putDescriptor = generator.next(someApiResponse).value;
+
+      expect(delayDescriptor).toMatchSnapshot();
+      expect(apiCallDescriptor).toMatchSnapshot();
+      expect(putDescriptor).toEqual(
+        put({
+          type: SIGNUP_SUCCESS,
+          payload: { token: expect.any(String), userId: expect.any(String) },
+        })
+      );
+      expect(generator.next().done).toBe(true);
     });
 
-    const delayDescriptor = generator.next().value;
+    it('handles unsuccessful signup', () => {
+      const someError = new Error('Bad Credentials');
+      const generator = signupSaga({
+        type: SIGNUP,
+        payload: { email: 'some-email@example.com', password: 'badpass' },
+      });
 
-    const apiCallDescriptor = generator.next().value;
+      const delayDescriptor = generator.next().value;
+      const apiCallDescriptor = generator.next().value;
+      const putDescriptor = generator.throw(someError).value;
 
-    const putDescriptor = generator.throw(someError).value;
-
-    expect(delayDescriptor).toMatchSnapshot();
-    expect(apiCallDescriptor).toMatchSnapshot();
-    expect(putDescriptor).toEqual(
-      put({ type: LOGIN_FAILURE, payload: someError.message })
-    );
-    expect(generator.next().done).toBe(true);
+      expect(delayDescriptor).toMatchSnapshot();
+      expect(apiCallDescriptor).toMatchSnapshot();
+      expect(putDescriptor).toEqual(
+        put({ type: SIGNUP_FAILURE, payload: someError.message })
+      );
+      expect(generator.next().done).toBe(true);
+    });
   });
 });
