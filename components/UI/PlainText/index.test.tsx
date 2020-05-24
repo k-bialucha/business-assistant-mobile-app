660fa44eb78448137e7ea242d6872faddbcbf77b
bastej <siatkasebastian@gmail.com>
2020-05-24 18:52:57 +0200

    Moving PlainText tests into same directory as component

--- components/UI/PlainText/index.test.tsx
+++ components/UI/PlainText/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { shallow } from 'enzyme';
 
-import PlainText from '../index';
+import PlainText from './index';
 
 describe('<PlainText />', () => {
   it('matches the snapshot', () => {
