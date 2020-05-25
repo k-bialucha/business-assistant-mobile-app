1c3e9be7edc0b747966d1b50b412ad0d9758c466
bastej <siatkasebastian@gmail.com>
2020-05-25 20:36:14 +0200

    Checking if it is the same without StartupScreen, comment added

--- screens/StartupScreen.tsx
+++ screens/StartupScreen.tsx
@@ -1,6 +1,8 @@
 /**
  * Startup module to check if user's data are saved in local storage, then log in automatically otherwise
  * set in redux didTryLogin to true
+ *
+ * it useEffect could be in NavContainer but then we see piece of whites screen after logout
  */
 
 import { useEffect } from 'react';
