c7e93084e0d786d5d22a5392e909d5fcaa499fdc
bastej <siatkasebastian@gmail.com>
2020-08-06 00:11:04 +0200

    Creating app Setting Screen with UI lib list element (showing logged user data)

--- src/screens/SettingsScreen/SettingsScreen.styled.ts
+++ src/screens/SettingsScreen/SettingsScreen.styled.ts
@@ -2,8 +2,6 @@ import styled from 'styled-components/native';
 
 const StyledView = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
 
 export { StyledView };
--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Alert, TouchableWithoutFeedback } from 'react-native';
 
-import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { ListItem } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { NavigationData } from '~/navigation/AppNavigator/SettingsNavigator';
 import { logout } from '~/redux/auth';
+import { RootState } from '~/redux/rootReducer';
+import Colors from '~/theme/Colors';
 
 import { StyledView } from './SettingsScreen.styled';
 
@@ -12,15 +16,65 @@ type Props = NavigationData<'Settings'>;
 
 const SettingsScreen: React.FC<Props> = () => {
   const dispatch = useDispatch();
+  const { username, userImage } = useSelector((state: RootState) => state.auth);
+  const { navigate } = useNavigation();
 
   return (
     <StyledView>
-      <Button
-        title="Log out"
+      <TouchableWithoutFeedback onPress={() => navigate('UserSettings')}>
+        <ListItem
+          // dummy strings temporary until load data into redux store during auto-login is not done
+          leftAvatar={{
+            rounded: true,
+            source: { uri: userImage || 'dummyUrl' },
+          }}
+          title={username || 'dummy name'}
+          titleStyle={{
+            color: Colors.navyBlue,
+            fontSize: 20,
+          }}
+          style={{ marginTop: 35, marginBottom: 35 }}
+          chevron
+        />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => Alert.alert('"Open view screen with some settings"')}
+      >
+        <ListItem
+          title="Test Option 1"
+          titleStyle={{
+            color: Colors.navyBlue,
+          }}
+          chevron
+          style={{ marginBottom: 5 }}
+        />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
+        onPress={() => Alert.alert('"Open view screen with some settings"')}
+      >
+        <ListItem
+          title="Test Option 2"
+          titleStyle={{
+            color: Colors.navyBlue,
+          }}
+          style={{ marginBottom: 5 }}
+          chevron
+        />
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback
         onPress={() => {
           dispatch(logout());
         }}
-      />
+      >
+        <ListItem
+          title="Log out"
+          titleStyle={{
+            color: Colors.errorRed,
+          }}
+          style={{ marginTop: 30 }}
+          contentContainerStyle={{ alignItems: 'center' }}
+        />
+      </TouchableWithoutFeedback>
     </StyledView>
   );
 };
