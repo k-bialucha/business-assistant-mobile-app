fa1c87ee8510202a9a247f47897bacbfe144c8ea
bastej <siatkasebastian@gmail.com>
2020-08-11 22:18:27 +0200

    Finding way to translate nav screen title

--- src/screens/UserSettingsScreen/UserSettingsScreen.tsx
+++ src/screens/UserSettingsScreen/UserSettingsScreen.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { StackNavigationOptions } from '@react-navigation/stack';
+import { Trans, useTranslation } from 'react-i18next';
 
 import PlainText from '~/components/UI/PlainText';
 import { NavigationData } from '~/navigation/AppNavigator/SettingsNavigator';
@@ -9,7 +10,14 @@ import { StyledView } from './UserSettingsScreen.styled';
 
 type Props = NavigationData<'UserSettings'>;
 
-const UserSettingsScreen: React.FC<Props> = () => {
+const UserSettingsScreen: React.FC<Props> = ({ navigation }) => {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // passing already translated title it could be common way to set nav options in entire app
+    navigation.setParams({ headerTitle: t('Account Settings') });
+  }, [t, navigation]);
+
   return (
     <StyledView>
       <PlainText theme="dark">User Settings Screen</PlainText>
@@ -17,8 +25,12 @@ const UserSettingsScreen: React.FC<Props> = () => {
   );
 };
 
-export const UserSettingsScreenNavOptions: StackNavigationOptions = {
-  headerTitle: 'Account Settings',
+export const UserSettingsScreenNavOptions: StackNavigationOptions = navData => {
+  // 1 - to avoid passing prop from component, but problem with headerTitle type, it expects string
+  // headerTitle: <Trans i18nKey="Account Settings" />,
+
+  // 2 set passed title, navData need type
+  return { title: navData.route.params?.headerTitle };
 };
 
 export default UserSettingsScreen;
