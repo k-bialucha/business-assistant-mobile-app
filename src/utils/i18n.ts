3696a9bb85ede2e0dcfec13896dabbd61c86dbc6
bastej <siatkasebastian@gmail.com>
2021-01-21 20:48:02 +0100

    Setting app language basing on device's region

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,8 +1,22 @@
+import * as Localization from 'expo-localization';
 import i18n from 'i18next';
+import { LanguageDetectorAsyncModule } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import { translationEN } from './translations/en';
 import { translationPL } from './translations/pl';
+
+const languageDetector: LanguageDetectorAsyncModule = {
+  type: 'languageDetector',
+  async: true,
+  detect: (callback: Function) => {
+    return Localization.getLocalizationAsync().then(({ locale }) => {
+      callback(locale.split('-')[0]);
+    });
+  },
+  init: () => ({}),
+  cacheUserLanguage: () => ({}),
+} as const;
 
 const resources = {
   en: {
@@ -14,15 +28,20 @@ const resources = {
 } as const;
 
 i18n
+  .use(languageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    fallbackLng: 'en',
+
     resources,
-    lng: 'en',
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
       escapeValue: false, // react already safes from xss
+    },
+    react: {
+      useSuspense: false, // fix issue with Suspense on Android
     },
   });
 
