fb8ad96db3f40b1a2adfa71a7d7bbdabef1894db
bastej <siatkasebastian@gmail.com>
2021-01-28 20:18:01 +0100

    Removing empty lines

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -18,9 +18,7 @@ i18n
   .init({
     resources,
     lng: 'en',
-
     keySeparator: false, // we do not use keys in form messages.welcome
-
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
