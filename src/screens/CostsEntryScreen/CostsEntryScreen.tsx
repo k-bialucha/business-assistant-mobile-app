406ea2e52e0249fbd4ed5147f6976844a0f7c8b9
bastej <siatkasebastian@gmail.com>
2021-02-10 22:36:10 +0100

    Implemeting select fields for Currency and Var Rate

--- src/screens/CostsEntryScreen/CostsEntryScreen.tsx
+++ src/screens/CostsEntryScreen/CostsEntryScreen.tsx
@@ -3,12 +3,14 @@ import { ScrollView } from 'react-native';
 
 import { Formik } from 'formik';
 import { Button } from 'react-native-elements';
+import RNPickerSelect from 'react-native-picker-select';
 import * as Yup from 'yup';
 
 import TextField from '~/components/form/TextField/TextField';
 import { useAppTranslation } from '~/hooks/useAppTranslation';
+import { AmountKind } from '~/models/AmountKind';
 import { Currency } from '~/models/Currency';
-import { VatRate } from '~/models/VatRate';
+import { VatRate, vatRates } from '~/models/VatRate';
 import { NavigationData } from '~/navigation/AppNavigator/CostsNavigator';
 import Colors from '~/theme/Colors';
 import {
@@ -21,26 +23,27 @@ type Props = NavigationData<'CostsEntry'>;
 type CostFormItem = {
   amount: string;
   amountKind: AmountKind;
-  currency: Currency;
-  vatRate: VatRate;
+  currency: Currency | '';
+  vatRate: VatRate | '';
   purchaseDate: string;
 };
-
-enum AmountKind {
-  NET = 'Net',
-  GROSS = 'Gross',
-}
 
 const CostsEntryScreen: React.FC<Props> = () => {
   const { t } = useAppTranslation();
 
-  const CostEntrySchema = Yup.object().shape({});
+  const CostEntrySchema = Yup.object<Partial<CostFormItem>>({
+    amount: Yup.string().required(t('Required')),
+    amountKind: Yup.string<AmountKind>().required(t('Required')),
+    currency: Yup.string<Currency>().required(t('Required')),
+    vatRate: Yup.string<VatRate>().required(t('Required')),
+    purchaseDate: Yup.string().required(t('Required')),
+  });
   const initialValues: CostFormItem = {
     amount: '',
     amountKind: AmountKind.GROSS, // 2x checkbox or switch
     // TODO: fill currency with base company currency in app?
-    currency: Currency.PLN, // select field
-    vatRate: '23', // select field
+    currency: '', // select field
+    vatRate: '', // select field
     purchaseDate: '', // date field
   };
 
@@ -62,6 +65,7 @@ const CostsEntryScreen: React.FC<Props> = () => {
             handleChange,
             handleBlur,
             handleSubmit,
+            setFieldTouched,
             values,
             isValid,
             errors,
@@ -85,22 +89,46 @@ const CostsEntryScreen: React.FC<Props> = () => {
                 error={errors.amountKind}
                 touched={touched.amountKind}
               />
-              <TextField
-                value={values.currency}
-                placeholder={t('Currency')}
-                onChangeText={handleChange('currency')}
-                onBlur={handleBlur('currency')}
-                error={errors.currency}
-                touched={touched.currency}
-              />
-              <TextField
-                value={values.vatRate}
-                placeholder={t('Vat Rate')}
-                onChangeText={handleChange('vatRate')}
-                onBlur={handleBlur('vatRate')}
-                error={errors.vatRate}
-                touched={touched.vatRate}
-              />
+              <RNPickerSelect
+                placeholder={{
+                  label: t('Select a currency'),
+                  value: '', // to override null and match Formik requirements
+                  color: 'gray',
+                }}
+                onValueChange={handleChange('currency')}
+                onClose={() => setFieldTouched('currency')} // imitating onBlur
+                items={Object.values(Currency).map(item => ({
+                  label: item,
+                  value: item,
+                }))}
+              >
+                <TextField
+                  value={values.currency}
+                  placeholder={t('Currency')}
+                  error={errors.currency}
+                  touched={touched.currency}
+                />
+              </RNPickerSelect>
+              <RNPickerSelect
+                placeholder={{
+                  label: t('Select a Vat Rate'),
+                  value: '', // to override null and match Formik requirements
+                  color: 'gray',
+                }}
+                onValueChange={handleChange('vatRate')}
+                onClose={() => setFieldTouched('vatRate')}
+                items={vatRates.map(item => ({
+                  label: `${item}%`,
+                  value: item,
+                }))}
+              >
+                <TextField
+                  value={values.vatRate ? `${values.vatRate}%` : values.vatRate}
+                  placeholder={t('Vat Rate')}
+                  error={errors.vatRate}
+                  touched={touched.vatRate}
+                />
+              </RNPickerSelect>
               <TextField
                 value={values.purchaseDate}
                 placeholder={t('Purchase Date')}
