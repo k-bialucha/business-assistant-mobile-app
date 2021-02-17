66759b118ee686fd55580e9279117675b814387a
bastej <siatkasebastian@gmail.com>
2021-02-18 00:30:14 +0100

    Implementaing new type of field - SelectField with using package

---
+++ src/components/form/SelectField/SelectField.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import RNPickerSelect, {
+  Item,
+  PickerSelectProps,
+} from 'react-native-picker-select';
+
+import { AppTheme } from '~/models/AppTheme ';
+
+import TextField from '../TextField/TextField';
+
+interface SelectFieldProps extends PickerSelectProps {
+  error?: string | boolean;
+  touched?: boolean;
+  theme?: AppTheme;
+  selectorPlaceholder?: Item | {};
+  placeholder?: string;
+}
+
+export const SelectField: React.FC<SelectFieldProps> = ({
+  error,
+  touched,
+  value,
+  placeholder,
+  selectorPlaceholder,
+  onValueChange,
+  onClose,
+  items,
+  theme,
+}) => {
+  return (
+    <RNPickerSelect
+      placeholder={{
+        label: selectorPlaceholder,
+        value: '', // to override null and match Formik requirements
+        color: 'gray',
+      }}
+      onValueChange={onValueChange}
+      onClose={onClose} // imitating onBlur
+      items={items}
+    >
+      <TextField
+        value={value}
+        placeholder={placeholder}
+        error={error}
+        touched={touched}
+        theme={theme}
+      />
+    </RNPickerSelect>
+  );
+};
