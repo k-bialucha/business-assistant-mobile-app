f20c913041161ad6bc6478b5441788c13a804b6f
bastej <siatkasebastian@gmail.com>
2021-03-02 23:41:32 +0100

    Fixing Segment Radio styles properties values

--- src/components/form/SegmentRadioField/SegmentRadioField.styled.ts
+++ src/components/form/SegmentRadioField/SegmentRadioField.styled.ts
@@ -1,25 +1,25 @@
 import styled from 'styled-components/native';
 
 const StyledView = styled.View`
-  display: 'flex';
-  width: '100%';
-  flex-direction: 'row';
-  padding-top: 18;
-  padding-bottom: 12;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  padding-top: 18px;
+  padding-bottom: 12px;
 `;
 
 const StyledTouchableOpacity = styled.TouchableOpacity`
-  display: 'flex';
+  display: flex;
   flex: 1;
-  border-width: 3;
-  align-items: 'center';
-  padding-vertical: 8;
-  padding-horizontal: 2;
+  border-width: 3px;
+  align-items: center;
+  padding-vertical: 8px;
+  padding-horizontal: 2px;
 `;
 
 const StyledText = styled.Text`
-  font-size: 18;
-  letter-spacing: 0.6;
+  font-size: 18px;
+  letter-spacing: 0.6px;
 `;
 
 export { StyledView, StyledTouchableOpacity, StyledText };
