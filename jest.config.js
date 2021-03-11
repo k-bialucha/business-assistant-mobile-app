d9f595a92bd6862ab540d7bcbcf5805b8cf98dfa
bastej <siatkasebastian@gmail.com>
2021-03-11 23:37:23 +0100

    Updated jest config to be able to test Select react-native-picker-select

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const config = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)',
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native-picker)',
   ],
 };
 
